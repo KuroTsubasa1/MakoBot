de42e2b78ccc0505d849dc0eea6200f70be6437c
Lasse Harm <lasseharm@web.de>
2017-09-27 08:39:00 +0200

    memes still not displayed

--- discordJs/memeGenerator.js
+++ discordJs/memeGenerator.js
@@ -1,6 +1,11 @@
 module.exports = {
 
   sendMeme: function(message, filename) {
-    message.channel.sendFile(`${filename}`)
+
+    // for local testing
+    // message.channel.sendFile(`${filename}`)
+
+    // for production
+    message.channel.sendFile(`/root/makobot/MakoBot/discordJs/${filename}`);
   }
 }
